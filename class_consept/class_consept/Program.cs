ac2ec15d0cc9e8a9834883390e26f7dbe2005d40
ahnaf <63817873+asimahnaf@users.noreply.github.com>
2020-11-25 17:51:16 +0600

    Add files via upload

---
+++ class_consept/class_consept/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_consept
+{
+    //public class Program3
+    //{
+    //    public string b;
+    //    public Program3()
+    //    {
+    //        Console.WriteLine("Welcome to Bangladesh");
+    //    }
+    //    public static void Main()
+    //    {
+    //        Program3 p = new Program3();
+    //        Program3 p2 = new Program3();
+    //        Console.ReadKey();
+    //    }
+    //}
+    ////*********************
+    //    class Program
+    //    {
+    //        static void Main(string[] args)
+    //        {
+
+
+    //        }
+    //    }
+    //    class staticvar
+    //    {
+    //        public static int num;
+    //        public void count()
+    //        {
+    //            num++
+    //        }
+    //        public int get num()
+    //        {
+    //            return num;
+    //        }
+
+    //    }
+    //    public class stacicvar
+    //    {
+    //        public static int num;
+
+    //        public void count()
+    //        {
+    //            num++;
+
+    //        }
+    //        public int getNum()
+    //        {
+    //            return num;
+    //        }
+
+    //    }
+    //    class staticTester 
+    //    { 
+    //        static void Main (string [] args)
+    //        {
+    //            stacicvar S1 = new stacicvar();
+    //            stacicvar S2 = new stacicvar();
+    //            S1.count();
+    //            S1.count();
+    //            S2.count();
+    //            S2.count();
+    //            Console.WriteLine("fon s1:{0}", num);
+    //            Console.WriteLine("fon s2:{0}", num);
+    //            Console.ReadKey();
+
+    //        }
+    //    }
+
+    //    class staticvar
+    //    {
+    //        public static int num;
+    //        public void count()
+    //        {
+    //            num++;
+    //        }
+    //        public int getnum()
+    //        {
+    //            return num;
+
+    //        }
+    //    }
+    //    class statictester
+    //    {
+
+    //        static void Main(string[] args)
+    //        {
+    //            staticvar S1 = new staticvar();
+    //            staticvar S2 = new staticvar();
+    //            S1.count();
+    //            S1.count();
+    //            S1.count();
+    //            Console.WriteLine("fon S1:{0}", staticvar.num);
+    //            Console.ReadLine();
+    //        }
+    //    }
+}
+
