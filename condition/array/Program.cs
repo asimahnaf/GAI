2f6b74b83d4e95ca33b475585b0e1e6d55c59097
ahnaf <63817873+asimahnaf@users.noreply.github.com>
2020-11-25 17:52:20 +0600

    Add files via upload

---
+++ condition/array/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace condition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*condiction dicleration*/ 
+            Console.WriteLine("choose your fast integer number a ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("choose your second integer number b ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            if (a>b)
+            {
+                Console.WriteLine("{0} is bigger then {1} ",a);
+            }
+            else
+            {
+                Console.WriteLine("{1} is a bigger than {0} ",b);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
